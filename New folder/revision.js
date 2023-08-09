6bb299b3a64b88d1d9a1ff1cc332c36e7c111898
0-shihab-0 <141819910+0-shihab-0@users.noreply.github.com>
2023-08-09 15:13:16 +0600

    Add files via upload

---
+++ New folder/revision.js
@@ -0,0 +1,46 @@
+console.log("assalamualaikum");
+
+ let name=" shihab"
+
+console.log(name[0]);
+console.log(name.length);
+console.log(name.length-4);
+
+console.log(name.trim());
+console.log(name.toLowerCase());
+console.log(name.toLowerCase().toUpperCase().trim());
+
+let Name=+"ashraf"
+
+console.log(typeof Name);
+
+
+let str="ashraf"
+let str1="khan"
+let str2="shihab"
+
+let full=str+" "+str1+" "+str2
+
+console.log(full);
+
+let age=19
+let nickName="shihab"
+
+let About_me=`my name is ${nickName} and my age is ${age}`
+console.log(About_me);
+
+
+let num1=3
+let num2=31
+console.log(num1==num2);
+console.log(num1===num2);
+console.log(num1!=num2);
+console.log(num1!==num2);
+
+if (age) {
+    
+} else {
+    
+}
+
+
