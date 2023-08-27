a0821a89d0eed8c7674d15167c4ca5d6a0aa418d
Ashraf Khan Shihab <141819910+0-shihab-0@users.noreply.github.com>
2023-08-27 11:04:59 +0600

    Add files via upload

---
+++ Push-Pop,Shift-Unshift.js
@@ -0,0 +1,24 @@
+// let fruits=["apple","banana","mango"]
+
+// console.log(fruits);
+
+// fruits.push("amm")
+// console.log(fruits);
+
+// let fr=fruits.push()
+
+// console.log(fr);
+
+
+// let removedFruits=fruits.pop();
+// console.log(fruits);
+
+// console.log(removedFruits);
+
+let fol=["apple","banana","mango"]
+
+// fol.unshift("banana")
+// console.log(fol);
+
+console.log(fol.shift());
+console.log(fol);
---
+++ revision.js
@@ -0,0 +1,200 @@
+// console.log("assalamualaikum");
+
+//  let name=" shihab"
+
+// console.log(name[0]);
+// console.log(name.length);
+// console.log(name.length-4);
+
+// console.log(name.trim());
+// console.log(name.toLowerCase());
+// console.log(name.toLowerCase().toUpperCase().trim());
+
+// let Name=+"ashraf"
+
+// console.log(typeof Name);
+
+
+// let str="ashraf"
+// let str1="khan"
+// let str2="shihab"
+
+// let full=str+" "+str1+" "+str2
+
+// console.log(full);
+
+// let age=19
+// let nickName="shihab"
+
+// let About_me=`my name is ${nickName} and my age is ${age}`
+// console.log(About_me);
+
+
+// let num1=3
+// let num2=31
+// console.log(num1==num2);
+// console.log(num1===num2);
+// console.log(num1!=num2);
+// console.log(num1!==num2);
+
+// let age=16
+// let name="shihab"
+// if (age>18) {
+//     console.log("you can play sanda");
+// } else {
+//     console.log("you can play taulu");
+// }
+
+// if (age>18) {
+//      (drink="coffee");
+// } else {
+//      (drink="milk");
+// }
+
+// console.log(drink);   
+
+// let age=16
+// let name="shihab"
+
+// let drink=age>18?"coffee":"milk"
+// console.log(drink);
+
+
+// let name="shihab"
+// let age=1
+
+
+// if (name[0]==="s") {
+//     console.log("your name start with s");
+// }
+// if (age>18) {
+//     console.log("you are above 18");
+// }
+
+// if (name[0]==="s" ||  age>18) {
+//     console.log("your name is start with s and ypu are above 18");
+// } else {
+//     console.log("something else");
+// }
+
+// let number=18
+//  let guess=+prompt("guess a number")
+// console.log(+guess);
+
+// if (guess12==number) {
+//     console.log("your guess is right");
+// } else {
+//     if (guess<number) {
+//         console.log("your guess is too low");
+//     } else {
+//         console.log("your guess is too high");
+//     }    
+// }
+
+
+// let temparature=49
+
+// if (temparature>45) {
+//     console.log("too hot outside.stay at home");
+// }else if(temparature>35){
+//     console.log("turn on ac");
+// }else if(temparature>25){
+//     console.log("let's go for a swim");
+// }else if(temparature>15){
+//     console.log("weather is okay");
+// }else if(temparature>0){
+//     console.log("it's cold outside");
+// }else{
+//     console.log("it's too cold");
+// }
+
+// let day=6
+
+// switch (day) {
+//     case 0:
+//         console.log("saturday");
+//         break;
+//      case 1:
+//         console.log("sunday");
+//         break;
+//         case 2:
+//             console.log("monday");
+//             break;
+//             case 3:
+//                 console.log("tuesday");
+//                 break;
+//                 case 4:
+//                     console.log("wednesday");
+//                     break;
+//                     case 5:
+//                         console.log("thursday");
+//                         break;
+//                         case 6:
+//                             console.log("friday");
+//                             break;
+//                             default:console.log("invalid day");
+// }
+
+
+// let i=0
+// console.log(i);
+// i++
+// console.log(i);
+// i++
+// console.log(i);
+// i++
+// console.log(i);
+// i++
+
+
+
+// let day=8
+// switch(day){
+// case 1:
+//         console.log("saturday");
+//         case 1:
+//     console.log("sunday");
+//     break;
+// case 2:
+//     console.log("monday");
+//     break;
+// case 3:
+//     console.log("tuesday");
+//     break;
+// case 4:
+//     console.log("wednesday");
+//     break;
+// case 5:
+//     console.log("thursday");
+//     break;
+// case 6:
+//     console.log("friday");
+//     break;
+// default:
+//         console.log("invalid day");
+// }
+
+
+// let i=1
+// while (i<4) {
+//     i++
+// }
+
+// console.log(i);
+
+let i=2
+let total=10
+while(i<10){
+    total=total+i;
+    i++
+}
+console.log(total);
+
+// for (let i=0;i<=9;i++){
+//     console.log(i);
+// }
+
+// console.log(i);
+
+
+
