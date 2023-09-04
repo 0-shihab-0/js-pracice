d356e3128ba988d475304f3e15b8674e6e3d4955
Ashraf Khan Shihab <141819910+0-shihab-0@users.noreply.github.com>
2023-09-04 14:45:21 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,66 @@
+// function salam(params) {
+//     console.log("assalamualaikum");
+// }
+// salam()
+
+
+// // function number(params) {
+// //     console.log(2+5);
+// // }
+// // number()
+
+
+// function number(params) {
+//     return 2+5;
+// }
+// console.log(number());
+
+// function number(number1,number2) {
+//     return 2+5;
+// }
+// console.log(number());
+
+// //odd even check
+
+function isEven(number){
+    if(number%2===0){
+        return true;
+    }
+   else {return false}
+}
+console.log(isEven(3));
+
+
+function isEven(number){
+    if(number%2===0){
+        return true;
+    }
+   return false
+}
+console.log(isEven(3));
+
+
+
+function isEven(number){
+    return number%2===0
+}
+console.log(isEven(3));
+
+
+
+function string(kuchbhi) {
+    return kuchbhi[0]
+}
+console.log(string("123"));
+
+
+
+//array and target
+
+
+
+
+
+
+
+
